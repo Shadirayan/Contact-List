f5c03d5d960780384a304e25cf4535af2cc5a1eb
Shadirayan <33546082+Shadirayan@users.noreply.github.com>
2019-07-21 19:20:47 +0300

    Added a header componenet

---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css';
+
+export class Header extends React.Component {
+    render() {
+      return (
+          <div>
+        <div className="nav-container">
+         <div className="nav">
+           <p>Hi</p>
+           <input type="text" name="search" className="input-style" placeholder="Search.."/>
+         </div>
+          </div>
+        </div>
+      );
+    }
+  }
